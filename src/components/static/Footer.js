6017b6cb235c50e370c41736b63e35f970580d94
Ruslan <ruslan777mirov@gmail.com>
2019-10-28 13:22:02 +0200

    Small fix

--- src/components/static/Footer.js
+++ src/components/static/Footer.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-import { Row, Col, Badge } from "react-bootstrap"
+import { Row, Col} from "react-bootstrap"
 
 class Footer extends Component {
   render() {
     return (
       <Row className="footer">
-      <Col><h5><Badge>Since 2018: world's first live EthFi investments funds marketplace</Badge></h5></Col>
+      <Col><strong>Since 2018: world's first live EthFi investments funds marketplace</strong></Col>
       </Row>
     )
   }
