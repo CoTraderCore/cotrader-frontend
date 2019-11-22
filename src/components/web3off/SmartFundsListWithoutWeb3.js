f979dd86d34b710d1d6893d9451eecb2d2992966
Awanti Kant Pandey <abaanoutsourcing@gmail.com>
2019-11-22 18:33:26 +0530

    View Fund Page chart layout fix

--- src/components/web3off/SmartFundsListWithoutWeb3.js
+++ src/components/web3off/SmartFundsListWithoutWeb3.js
@@ -101,8 +101,6 @@ class SmartFundsListWithoutWeb3 extends Component{
             </div>
             </Row>
 
-             <br />
-
              <FundsNav />
              <ListGroup variant="flush">
              {
