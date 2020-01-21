b951ef2c12dd7b6ce80a2e2dac991c9ab7367b8a
Ruslan <ruslan777mirov@gmail.com>
2020-01-21 12:30:21 +0200

    Finish setRate in SwapPool.js

--- src/components/actions/Pool/SwapPool.js
+++ src/components/actions/Pool/SwapPool.js
@@ -1,20 +1,96 @@
 import React, { Component } from 'react'
-import { Button, Form } from "react-bootstrap"
+import { Button, Form, Alert } from "react-bootstrap"
 import { Typeahead } from 'react-bootstrap-typeahead'
-import { SmartFundABIV3 } from '../../../config.js'
-
+import {
+  SmartFundABIV3,
+  PoolPortalABI,
+  PoolPortal,
+  ERC20ABI
+} from '../../../config.js'
+import { toWeiByDecimalsInput, fromWeiByDecimalsInput } from '../../../utils/weiByDecimals'
+import { isAddress } from 'web3-utils'
 
 class SwapPool extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
-      toAddress:'',
       fromAddress:'',
+      toAddress:'',
       isToERC20:false,
       isFromERC20:false,
-      amount:0
+      amount:0,
+      recieve:0,
+      reciveFromWei:0,
+      symbolTo: ''
     }
   }
+
+  // update rate by onChange
+  componentDidUpdate(prevProps, prevState){
+    if(prevState.toAddress !== this.state.toAddress
+      ||
+      prevState.fromAddress !== this.state.fromAddress
+      ||
+      prevState.amount !== this.state.amount
+    )
+    {
+      if(this.state.amount > 0 && isAddress(this.state.fromAddress) && isAddress(this.state.toAddress))
+      this.setRate()
+    }
+  }
+
+  // get ration between fron and to
+  setRate = async () => {
+    const web3 = this.props.web3
+    const poolPortal = new web3.eth.Contract(PoolPortalABI, PoolPortal)
+
+    // Get amount in wei by decimals for send
+    const fromTokenInfo = await this.getTokenInfo(this.state.fromAddress)
+    const decimalsFrom = fromTokenInfo.decimals
+    const amountInWei = toWeiByDecimalsInput(decimalsFrom, this.state.amount)
+
+    // get rate
+    const recive = await poolPortal.methods.getRatio(
+    this.state.fromAddress,
+    this.state.toAddress,
+    amountInWei
+    ).call()
+
+    // Convert recieve in wei
+    const toTokenInfo = await this.getTokenInfo(this.state.toAddress)
+    const decimalsTo = toTokenInfo.decimals
+    const reciveFromWei = Number(fromWeiByDecimalsInput(decimalsTo, recive))
+
+    const symbolTo = toTokenInfo.symbol
+
+    this.setState({ recive, reciveFromWei, symbolTo })
+  }
+
+  checkFundBalance = async () => {
+    
+  }
+
+  // execude swap
+  swap = () => {
+
+  }
+
+  // get decimal and symbol by token address
+  getTokenInfo = async (tokenAddress) => {
+    const web3 = this.props.web3
+    const token = new web3.eth.Contract(ERC20ABI, tokenAddress)
+    const decimals = await token.methods.decimals().call()
+
+    let symbol
+    try{
+      symbol = await token.methods.symbol().call()
+    }catch(e){
+      symbol = "Token"
+    }
+
+    return { decimals, symbol }
+  }
+
 
   render() {
     return (
@@ -63,7 +139,16 @@ class SwapPool extends Component {
       onChange={(e) => this.setState({ amount: e.target.value })}
       type="number" min="1"/>
       <br/>
-      <Button variant="outline-primary">Swap</Button>
+      {
+        this.state.reciveFromWei > 0
+        ?
+        (
+          <Alert variant="success">You will recive : {this.state.reciveFromWei} {this.state.symbolTo}</Alert>
+        )
+        :null
+      }
+      <br/>
+      <Button variant="outline-primary" onClick={() => this.swap()}>Swap</Button>
       </React.Fragment>
     )
   }
