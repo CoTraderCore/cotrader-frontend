9f35511e811e31e252266082c272061b7a046f5b
Ruslan <ruslan777mirov@gmail.com>
2020-07-16 14:10:41 +0300

    Add multiple connectors support in Bancor/SellPool

--- src/components/actions/Pool/Bancor/SellPool.js
+++ src/components/actions/Pool/Bancor/SellPool.js
@@ -10,17 +10,18 @@ import {
 
 import { toWeiByDecimalsInput, fromWeiByDecimalsInput } from '../../../../utils/weiByDecimals'
 import setPending from '../../../../utils/setPending'
-import { toWei } from 'web3-utils'
+import { toWei, fromWei } from 'web3-utils'
+// Fund recognize ETH by this address
+const ETH = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'
+
+
 
 class SellPool extends Component {
   constructor(props, context) {
     super(props, context);
     this.state = {
       amount:0,
-      bntAmountFromWei:0,
-      ercAmountFromWei:0,
-      ercSymbol:'',
-      bntSymbol:'',
+      connectorsDATA:[],
       isEnoughBalance:false,
       isComputed:false,
       ErrorText:''
@@ -38,49 +39,56 @@ class SellPool extends Component {
   updateInfo = async () => {
     if(this.props.fromAddress && this.state.amount > 0){
       const web3 = this.props.web3
+      const poolAddress = this.props.fromAddress
+      
       // get current reserve amount for pool
       const poolPortal = new web3.eth.Contract(PoolPortalABI, PoolPortal)
 
-      // get addresses
-      const { BNTConnector, ERCConnector } = await poolPortal.methods.getBancorConnectorsByRelay(
-        this.props.fromAddress
+      // get connectors address
+      const connectors = await poolPortal.methods.getBancorConnectorsByRelay(
+        poolAddress
       ).call()
 
-      // get amount
-      const { bancorAmount, connectorAmount } = await poolPortal.methods.getBancorConnectorsAmountByRelayAmount(
-        toWei(String(this.state.amount)),
-        this.props.fromAddress
-      ).call()
+      let connectorAmount
+      let connectorAmountFromWei
+      let symbol
+      const connectorsDATA = []
 
-      // get ERC conenctots contract
-      const bntToken = new web3.eth.Contract(ERC20ABI, BNTConnector)
-      const ercToken = new web3.eth.Contract(ERC20ABI, ERCConnector)
+      for(let i = 0; i < connectors.length; i++){
+        // get connector amount needs
+        connectorAmount = await poolPortal.methods.getBancorConnectorsAmountByRelayAmount(
+          toWei(String(this.state.amount)),
+          poolAddress,
+          connectors[i]).call()
 
-      // get symbols
-      // try catch for bytes32 return case
-      let ercSymbol
-      try{
-        ercSymbol = await ercToken.methods.symbol().call()
-      }catch(e){
-        ercSymbol = "ERC"
+          // ETH case
+          if(String(connectors[i]).toLowerCase() === String(ETH).toLowerCase()){
+            symbol = "ETH"
+            connectorAmountFromWei = fromWei(connectorAmount)
+          }
+          // ERC20 case
+          else{
+            const tokenContract = new web3.eth.Contract(ERC20ABI, connectors[i])
+            try{
+              symbol = await tokenContract.methods.symbol().call()
+            }catch(e){
+              symbol = "ERC"
+            }
+
+            connectorAmountFromWei = connectorAmount > 0
+            ? fromWeiByDecimalsInput(await tokenContract.methods.decimals().call(), connectorAmount)
+            : 0
+          }
+
+          connectorsDATA.push({ symbol, connectorAmount, connectorAmountFromWei })
       }
-
-      const bntSymbol = await bntToken.methods.symbol().call()
-
-      // get amount
-      const ercDecimals = await ercToken.methods.decimals().call()
-      const bntAmountFromWei = fromWeiByDecimalsInput(18, bancorAmount)
-      const ercAmountFromWei = fromWeiByDecimalsInput(ercDecimals, connectorAmount)
 
       // check fund balance
       const { fundBalanceFromWei } = await this.getFundBalance()
       const isEnoughBalance = fundBalanceFromWei >= this.state.amount ? true : false
 
       this.setState({
-        bntAmountFromWei,
-        ercAmountFromWei,
-        ercSymbol,
-        bntSymbol,
+        connectorsDATA,
         isEnoughBalance,
         isComputed:true
       })
@@ -89,10 +97,7 @@ class SellPool extends Component {
 
   resetInfo(){
     this.setState({
-      bntAmountFromWei:0,
-      ercAmountFromWei:0,
-      ercSymbol:'',
-      bntSymbol:'',
+      connectorsDATA:[],
       isEnoughBalance:false,
       isComputed:false,
       ErrorText:''
@@ -223,15 +228,21 @@ class SellPool extends Component {
       <br/>
       <br/>
       {
-        this.state.bntAmountFromWei !== 0 && this.state.ercAmountFromWei !== 0
+        this.state.connectorsDATA.length > 0
         ?
         (
           <Alert variant="success">
-          You will receive
-          &nbsp;
-          {this.state.bntSymbol}&nbsp;:&nbsp;{this.state.bntAmountFromWei}
-          &nbsp;&nbsp;
-          {this.state.ercSymbol}&nbsp;:&nbsp;{this.state.ercAmountFromWei}
+          You will receive:
+          <hr/>
+          {
+            this.state.connectorsDATA.map((item, key) => {
+              return(
+                <p key={key}>
+                {item.symbol}&nbsp;:&nbsp;{item.connectorAmountFromWei}
+                </p>
+              )
+            })
+          }
           </Alert>
         ):null
       }
