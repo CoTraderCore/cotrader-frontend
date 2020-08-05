943b1322bb10b71e9d62bd4ea070a4865eda4bcd
Ruslan <ruslan777mirov@gmail.com>
2020-08-05 06:17:10 +0300

    Update BuyV2Pool

--- src/components/actions/Pool/Bancor/BuyV2Pool.js
+++ src/components/actions/Pool/Bancor/BuyV2Pool.js
@@ -4,15 +4,13 @@
 import React, { PureComponent } from 'react'
 import { Form, Button, Alert } from "react-bootstrap"
 import {
-  PoolPortalV6,
-  PoolPortalABIV7,
   SmartFundABIV7,
   BancorConverterABI,
   ERC20ABI,
   ERC20Bytes32ABI,
-  EtherscanLink
 } from '../../../../config.js'
-
+import setPending from '../../../../utils/setPending'
+import { toWeiByDecimalsInput } from '../../../../utils/weiByDecimals'
 
 class BuyV2Pool extends PureComponent {
   constructor(props, context) {
@@ -30,6 +28,40 @@ class BuyV2Pool extends PureComponent {
   componentDidUpdate(prevProps, prevState){
     if(prevProps.converterAddress !== this.props.converterAddress)
       this.updateConnectorsData()
+  }
+
+  addLiquidity = async () => {
+    const connectorsAddress = this.state.connectors.map(item => item.address)
+    const connectorsAmount = this.state.connectors.map(item => item.amount)
+    const smartFund = new this.props.web3.eth.Contract(SmartFundABIV7, this.props.smartFundAddress)
+
+    // pack data in bytes
+    const data = this.props.web3.eth.abi.encodeParameters(
+      ['address[]', 'uint256[]', 'uint256'],
+      [connectorsAddress, connectorsAmount, 1]
+    )
+
+    // get gas price from local storage
+    const gasPrice = localStorage.getItem('gasPrice') ? localStorage.getItem('gasPrice') : 2000000000
+    const block = await this.props.web3.eth.getBlockNumber()
+
+    // buy pool
+    smartFund.methods.buyPool(
+      0, // for Bancor v2 we calculate pool amount by connectors
+      0, // type Bancor
+      this.props.fromAddress, // pool address
+      connectorsAddress,
+      connectorsAmount,
+      ['0x000000000000000000000000000000000000000000000000000000000000001c'], // TODO convert converter version to bytes32
+      data
+    )
+    .send({ from:this.props.accounts[0], gasPrice })
+    .on('transactionHash', (hash) => {
+    // pending status for spiner
+    this.props.pending(true)
+    // pending status for DB
+    setPending(this.props.smartFundAddress, 1, this.props.accounts[0], block, hash, "Trade")
+    })
   }
 
   // get connectors by converter address
@@ -57,7 +89,7 @@ class BuyV2Pool extends PureComponent {
     return item.symbol === symbol
     })
     // TODO: convert  to wei by decimals
-    searchObj[0].amount = amount
+    searchObj[0].amount = toWeiByDecimalsInput(searchObj[0].decimals, amount)
     console.log(this.state.connectors)
   }
 
@@ -112,6 +144,7 @@ class BuyV2Pool extends PureComponent {
               )
             })
           }
+          <Button variant="outline-primary" onClick={() => this.addLiquidity()}>Buy</Button>
           </Form>
         )
         :null
